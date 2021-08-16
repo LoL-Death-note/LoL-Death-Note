343496de1c966d6dd418186bd6ca5e5d798bdfff
김범진 <sdpthf@naver.com>
2021-08-16 22:55:56 +0900

    DeathNoteApplicationTests

---
+++ death-note/src/test/java/com/example/deathnote/DeathNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deathnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeathNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
